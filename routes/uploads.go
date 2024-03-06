2d8a48fc48e88851c1bbec795b5d8953ce9bab46
Stephan Du Toit <asdutoit@gmail.com>
2024-03-06 13:25:49 +0200

    Updated uploads to upload images to AWS

--- routes/uploads.go
+++ routes/uploads.go
@@ -6,11 +6,27 @@ import (
 	"net/http"
 	"strings"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/gin-gonic/gin"
 )
 
 func uploadFiles(ctx *gin.Context) {
-	err := ctx.Request.ParseMultipartForm(10 << 20) // 10 MB limit
+	region := "af-south-1"
+
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String(region),
+	})
+	if err != nil {
+		fmt.Println("Error creating session:", err)
+		return
+	}
+	svc := s3.New(sess)
+
+	bucket := "homerunner-nextjs13-template"
+
+	err = ctx.Request.ParseMultipartForm(10 << 20) // 10 MB limit
 	if err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
@@ -30,11 +46,28 @@ func uploadFiles(ctx *gin.Context) {
 			return
 		}
 
-		err := ctx.SaveUploadedFile(file, "uploads/"+file.Filename)
+		// Open the file
+		src, err := file.Open()
 		if err != nil {
 			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 			return
 		}
+		defer src.Close()
+
+		// Upload the file to AWS S3
+		err = uploadToAWS(svc, bucket, file.Filename, src)
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		// Save the file to the server
+
+		// err = ctx.SaveUploadedFile(file, "uploads/"+file.Filename)
+		// if err != nil {
+		// 	ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		// 	return
+		// }
 	}
 
 	ctx.JSON(http.StatusOK, gin.H{"message": "Files uploaded successfully"})
@@ -61,4 +94,16 @@ func getContentType(file *multipart.FileHeader) string {
 	// Sniff the content type
 	mimeType := http.DetectContentType(buffer)
 	return mimeType
+}
+
+func uploadToAWS(svc *s3.S3, bucket, key string, file multipart.File) error {
+	_, err := svc.PutObject(&s3.PutObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+		Body:   file,
+	})
+	if err != nil {
+		return err
+	}
+	return nil
 }
